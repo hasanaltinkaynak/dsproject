b5ed566efabb62639fa281eb2a0322bf1a9adff0
EC2 Default User <ec2-user@ip-172-17-1-237.ec2.internal>
2022-08-05 02:50:45 +0000

    firstt commit

---
+++ install_models.py
@@ -0,0 +1,6 @@
+import nltk
+
+if __name__ == '__main__':
+    nltk.download('punkt',download_dir='/usr/lib/')
+    nltk.download('cmudict',download_dir='/usr/lib/')
+    nltk.download('wordnet',download_dir='/usr/lib/')
